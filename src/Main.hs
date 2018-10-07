b61827b130632dddba88ab5f07c8cb0a4c77c5d0
hamilton108 <hamilton108@palenque.com>
2018-10-07 21:31:39 +0200

    Better function names

--- src/Main.hs
+++ src/Main.hs
@@ -15,27 +15,31 @@ import Text.Printf (printf)
 import Data.Aeson (Value)
 import qualified Data.ByteString.Char8 as B
 
+import qualified Data.List as L
+-- import qualified System.IO.Encoding as IO
 {-|
-    downLoadPaperHistory returns a response BsResponse from the url like (f.ex NHY):
+    downloadPaperHistory returns a response BsResponse from the url like (f.ex NHY):
     
         http://www.netfonds.no/quotes/paperhistory.php?paper=NHY.OSE&csv_format=csv
-
-    and writes it to a csv file (f.ex NHY):
-
-        NHY.csv
 -}
-downLoadPaperHistory :: String -> R.Req R.BsResponse
-downLoadPaperHistory ticker = 
+downloadPaperHistory :: String -> R.Req R.BsResponse
+downloadPaperHistory ticker = 
     let 
         tickerParam = printf "%s.OSE" ticker 
         params = "paper" =: (pack tickerParam) <> "csv_format" =: ("csv" :: Text)
     in
     R.req R.GET (R.http "netfonds.no" /: "quotes" /: "paperhistory.php") R.NoReqBody R.bsResponse params 
 
+{-|
+    savePaperHistory gets a http response from downloadPaperHistory 
+    and writes it to a csv file (f.ex NHY):
+
+        NHY.csv
+-}
 savePaperHistory :: String -> IO ()
 savePaperHistory ticker = 
     R.runReq def $
-    downLoadPaperHistory ticker >>= \bs -> 
+    downloadPaperHistory ticker >>= \bs -> 
     liftIO $ B.writeFile (printf "%s.csv" ticker) (R.responseBody bs)
 
 main :: IO ()
