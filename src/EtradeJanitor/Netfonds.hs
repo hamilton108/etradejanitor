83856085e2ec2812bc93b054b174270b47539069
hamilton108 <hamilton108@palenque.com>
2018-10-08 23:13:05 +0200

    Netfonds.hs

---
+++ src/EtradeJanitor/Netfonds.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module EtradeJanitor.Netfonds where
+
+import Data.Text (Text,pack)
+import Text.Printf (printf)
+import Control.Monad.IO.Class (liftIO)
+import Data.Default.Class (def)
+import Network.HTTP.Req ((=:), (/:))
+import qualified Network.HTTP.Req as R
+import qualified Data.ByteString.Char8 as B
+
+{-|
+    downloadPaperHistory returns a response BsResponse from the url like (f.ex NHY):
+
+        http://www.netfonds.no/quotes/paperhistory.php?paper=NHY.OSE&csv_format=csv
+-}
+downloadPaperHistory :: String -> R.Req R.BsResponse
+downloadPaperHistory ticker =
+    let
+        tickerParam = printf "%s.OSE" ticker
+        params = "paper" =: (pack tickerParam) <> "csv_format" =: ("csv" :: Text)
+    in
+    R.req R.GET (R.http "netfonds.no" /: "quotes" /: "paperhistory.php") R.NoReqBody R.bsResponse params
+
+{-|
+    savePaperHistory gets a http response from downloadPaperHistory
+    and writes it to a csv file (f.ex NHY):
+
+        NHY.csv
+-}
+savePaperHistory :: String -> IO ()
+savePaperHistory ticker =
+    R.runReq def $
+    downloadPaperHistory ticker >>= \bs ->
+    liftIO $ B.writeFile (printf "%s.csv" ticker) (R.responseBody bs)
--- src/Main.hs
+++ src/Main.hs
@@ -16,9 +16,9 @@ import Data.Aeson (Value)
 import qualified Data.ByteString.Char8 as B
 
 import qualified Data.List as L
--- import qualified System.IO.Encoding as IO
 import Control.Monad (forM_)
 import Data.List.Split (splitOn)
+import EtradeJanitor.Netfonds as NP
 
 
 import System.IO
@@ -51,31 +51,6 @@ main2 = do
     putStrLn "OK"
 
 
-{-|
-    downloadPaperHistory returns a response BsResponse from the url like (f.ex NHY):
-
-        http://www.netfonds.no/quotes/paperhistory.php?paper=NHY.OSE&csv_format=csv
--}
-downloadPaperHistory :: String -> R.Req R.BsResponse
-downloadPaperHistory ticker =
-    let
-        tickerParam = printf "%s.OSE" ticker
-        params = "paper" =: (pack tickerParam) <> "csv_format" =: ("csv" :: Text)
-    in
-    R.req R.GET (R.http "netfonds.no" /: "quotes" /: "paperhistory.php") R.NoReqBody R.bsResponse params
-
-{-|
-    savePaperHistory gets a http response from downloadPaperHistory
-    and writes it to a csv file (f.ex NHY):
-
-        NHY.csv
--}
-savePaperHistory :: String -> IO ()
-savePaperHistory ticker =
-    R.runReq def $
-    downloadPaperHistory ticker >>= \bs ->
-    liftIO $ B.writeFile (printf "%s.csv" ticker) (R.responseBody bs)
-
 main :: IO ()
 main =
-    savePaperHistory "NHY"
+    NP.savePaperHistory "NHY"
