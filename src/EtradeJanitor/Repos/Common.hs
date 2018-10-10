ce6e242cc04062cae07d9731ff0ae3bc846a41e0
hamilton108 <hamilton108@palenque.com>
2018-10-10 20:57:41 +0200

    Refactored common Hasql methods to Repos.Common

--- src/EtradeJanitor/Repos/Common.hs
+++ src/EtradeJanitor/Repos/Common.hs
@@ -1,9 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module EtradeJanitor.Repos.Common (conn,SessionError(..)) where
+module EtradeJanitor.Repos.Common (conn,plain,SessionError(..)) where
 
+import Control.Monad.Except (lift,runExceptT,ExceptT(..))
+import Data.Either.Combinators (mapLeft)
+import qualified Data.ByteString.Char8 as B
+
+-- Hasql
 import qualified Hasql.Connection as HC
 import qualified Hasql.Session as HS
+import qualified Hasql.Statement as HQ
+import qualified Hasql.Decoders as HD
+
+-- Local
 
 conn :: HC.Settings
 conn = HC.settings "172.17.0.2" 5432 "trader" "ok" "trader"
@@ -12,3 +21,21 @@ data SessionError =
   ConnErr (HC.ConnectionError) |
   SessionError (HS.QueryError)
   deriving (Show, Eq)
+
+
+plain :: B.ByteString -> HQ.Statement () ()
+plain sql =
+  HQ.Statement sql mempty HD.unit False
+
+session :: HS.Session a -> IO (Either SessionError a)
+session session =
+  runExceptT $ acquire >>= \connection -> use connection <* release connection
+  where
+    acquire =
+      ExceptT $ fmap (mapLeft ConnErr) $ HC.acquire conn
+    use connection =
+      ExceptT $
+      fmap (mapLeft SessionError) $
+      HS.run session connection
+    release connection =
+      lift $ HC.release connection
--- src/EtradeJanitor/Repos/PaperHistoryRepos.hs
+++ src/EtradeJanitor/Repos/PaperHistoryRepos.hs
@@ -23,38 +23,12 @@ import qualified EtradeJanitor.Common.Types as T
 
 -- oid | ticker_id |     dx     |  opn  |  hi   |  lo   |  cls  |   vol
 
-demo :: IO ()
-demo = HC.acquire C.conn >>= \c ->
-    let msg = case c of
-            Right cx -> "YES"
-            Left err -> case err of
-                            Just xxx -> B.unpack xxx
-                            Nothing -> "??????"
-
-    in
-    putStrLn msg
-
-plain :: B.ByteString -> HQ.Statement () ()
-plain sql =
-  HQ.Statement sql mempty HD.unit False
 
 insertRow :: HS.Session ()
 insertRow =
-    HS.statement () $ plain $
-    "insert into ax (ar) values (1)"
+    HS.statement () $ C.plain $
+    "insert into stockmarket.ax (ar) values (1)"
 
-session :: HS.Session a -> IO (Either C.SessionError a)
-session session =
-  runExceptT $ acquire >>= \connection -> use connection <* release connection
-  where
-    acquire =
-      ExceptT $ fmap (mapLeft C.ConnErr) $ HC.acquire C.conn
-    use connection =
-      ExceptT $
-      fmap (mapLeft C.SessionError) $
-      HS.run session connection
-    release connection =
-      lift $ HC.release connection
 
 -- data Person =
 --     Person { name :: Text, gender :: Gender, age :: Int }
