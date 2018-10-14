6f6c095fc20a3c57894f97ab979e60efb2d77f9a
hamilton108 <hamilton108@palenque.com>
2018-10-14 16:36:36 +0200

    type Tickers = Vector Ticker

--- src/EtradeJanitor/Common/Types.hs
+++ src/EtradeJanitor/Common/Types.hs
@@ -1,11 +1,16 @@
 module EtradeJanitor.Common.Types where
 
-import Text.Printf (formatString,PrintfArg(..))
+import qualified Text.Printf as TP -- (formatString,PrintfArg(..))
+import qualified Data.Int as DI
+import qualified Data.Text as Tx
+import qualified Data.Vector as DV
 
-data Ticker = Ticker Int String deriving (Show)
+data Ticker = Ticker DI.Int64 Tx.Text deriving (Show)
 
-instance PrintfArg Ticker where
-  formatArg (Ticker _ t) fmt = formatString t fmt
+type Tickers = DV.Vector Ticker
+
+instance TP.PrintfArg Ticker where
+  formatArg (Ticker _ t) fmt = TP.formatString (Tx.unpack t) fmt
 
 data StockPrice =
   StockPrice {
--- src/EtradeJanitor/Repos/Stocks.hs
+++ src/EtradeJanitor/Repos/Stocks.hs
@@ -17,50 +17,18 @@ import qualified Data.Vector as DV
 import qualified EtradeJanitor.Repos.Common as C
 import qualified EtradeJanitor.Common.Types as T
 
-tickers :: IO [T.Ticker]
-tickers = undefined
 
-selectSum :: HST.Statement (Int64, Int64) Int64
-selectSum =
-  HST.Statement sql encoder decoder True
-  where
-    sql =
-      "select ($1 + $2)"
-    encoder =
-      contramap fst (HE.param HE.int8) <>
-      contramap snd (HE.param HE.int8)
-    decoder =
-      HD.singleRow (HD.column HD.int8)
+tickers :: IO (Either C.SessionError T.Tickers)
+tickers =
+  C.session $
+    HS.statement () selectStockTickers
 
-selectCount :: HST.Statement ()  Int64
-selectCount =
-  HST.Statement sql encoder decoder False
-  where
-    sql =
-      "select count(1) from stockmarket.stockprice"
-    encoder =
-      HE.unit
-    decoder =
-      HD.singleRow (HD.column HD.int8)
 
-selectCountTickers :: HST.Statement () [Int64]
-selectCountTickers =
-  HST.Statement sql encoder decoder False
-  where
-    sql =
-      "select ticker_id,count(1) from stockmarket.stockprice group by ticker_id"
-    encoder =
-      HE.unit
-    decoder =
-      HD.rowList (HD.column HD.int8)
 
--- stockTickerDecoder :: HD.Row T.Ticker
--- stockTickerDecoder = T.Ticker <$> HD.column HD.int8 <*> HD.column HD.text
+stockTickerDecoder :: HD.Row T.Ticker -- (Int64, Text)
+stockTickerDecoder = T.Ticker <$> HD.column HD.int8 <*> HD.column HD.text
 
-stockTickerDecoder :: HD.Row (Int64, Text)
-stockTickerDecoder = (,) <$> HD.column HD.int8 <*> HD.column HD.text
-
-selectStockTickers :: HST.Statement () (DV.Vector (Int64,Text))
+selectStockTickers :: HST.Statement () (DV.Vector T.Ticker)  -- (DV.Vector (Int64,Text))
 selectStockTickers =
   HST.Statement sql encoder decoder False
   where
@@ -73,29 +41,63 @@ selectStockTickers =
       -- HD.rowVector (HD.column HD.int8)
       HD.rowVector stockTickerDecoder
 
-demo =
-  C.session $
-        HS.statement () selectStockTickers
+-- demo =
+--   C.session $
+--         HS.statement () selectStockTickers
 
-data Person =
-    Person { name :: Text, gender :: Gender, age :: Int }
-    deriving (Show)
+-- selectSum :: HST.Statement (Int64, Int64) Int64
+-- selectSum =
+--   HST.Statement sql encoder decoder True
+--   where
+--     sql =
+--       "select ($1 + $2)"
+--     encoder =
+--       contramap fst (HE.param HE.int8) <>
+--       contramap snd (HE.param HE.int8)
+--     decoder =
+--       HD.singleRow (HD.column HD.int8)
+--
+-- selectCount :: HST.Statement ()  Int64
+-- selectCount =
+--   HST.Statement sql encoder decoder False
+--   where
+--     sql =
+--       "select count(1) from stockmarket.stockprice"
+--     encoder =
+--       HE.unit
+--     decoder =
+--       HD.singleRow (HD.column HD.int8)
+--
+-- selectCountTickers :: HST.Statement () [Int64]
+-- selectCountTickers =
+--   HST.Statement sql encoder decoder False
+--   where
+--     sql =
+--       "select ticker_id,count(1) from stockmarket.stockprice group by ticker_id"
+--     encoder =
+--       HE.unit
+--     decoder =
+--       HD.rowList (HD.column HD.int8)
 
-data Gender =
-    Male | Female
-    deriving (Show)
-
-personParams :: HE.Params Person
-personParams =
-    contramap name (HE.param HE.text) <>
-    contramap gender (HE.param genderValue) <>
-    contramap (fromIntegral . age) (HE.param HE.int8)
-
-genderValue :: HE.Value Gender
-genderValue =
-    contramap genderText HE.text
-    where
-        genderText ge =
-            case ge of
-                Male -> "male"
-                Female -> "female"
+-- data Person =
+--     Person { name :: Text, gender :: Gender, age :: Int }
+--     deriving (Show)
+--
+-- data Gender =
+--     Male | Female
+--     deriving (Show)
+--
+-- personParams :: HE.Params Person
+-- personParams =
+--     contramap name (HE.param HE.text) <>
+--     contramap gender (HE.param genderValue) <>
+--     contramap (fromIntegral . age) (HE.param HE.int8)
+--
+-- genderValue :: HE.Value Gender
+-- genderValue =
+--     contramap genderText HE.text
+--     where
+--         genderText ge =
+--             case ge of
+--                 Male -> "male"
+--                 Female -> "female"
