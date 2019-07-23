624a21d8cf4723bb4293d865b933f58db7b0086d
Hamilton 108 <hamilton@hamilton.com>
2019-07-23 12:58:04 +0200

    fetchStockPrices skeleton

--- src/EtradeJanitor/Repos/PaperHistoryEuroInvestor.hs
+++ src/EtradeJanitor/Repos/PaperHistoryEuroInvestor.hs
@@ -2,4 +2,11 @@
 
 module EtradeJanitor.Repos.PaperHistoryEuroInvestor where
 
-a = 3
+import qualified Text.HTML.TagSoup as TS
+
+import qualified EtradeJanitor.Common.Types as T
+
+type StringSoup = [TS.Tag String]
+
+fetchStockPrices :: T.Ticker -> IO [T.StockPrice]
+fetchStockPrices tix = pure []
