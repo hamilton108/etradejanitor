e092a9f4953925ffae64ad5cd8f7aec62c70efd3
Hamilton 108 <hamilton@hamilton.com>
2019-09-10 23:13:01 +0200

    (=:) operator compiles

--- src/Demo/Demo1.hs
+++ src/Demo/Demo1.hs
@@ -2,10 +2,42 @@
 
 module Demo.Demo1 where
 
+import Control.Monad.IO.Class (liftIO)
+
+import Data.Maybe (fromJust)
 import Data.Text as Text
-import Network.HTTP.Req ((/:),(=:))
-import qualified Network.HTTP.Req as Req
+-- import Network.HTTP.Req ((/:),(=:))
+-- import qualified Network.HTTP.Req as Req
+import Network.HTTP.Req
 import qualified Data.ByteString.Char8 as Char8
 
-p :: Text.Text -> Char8.ByteString -> Req.QueryParam
-p = "a" =: "b" 
+ {-
+main :: IO ()
+main = runReq defaultHttpConfig $ do
+    -- This is an example of what to do when URL is given dynamically. Of
+    -- course in a real application you may not want to use 'fromJust'.
+    let (url, options) = fromJust (parseUrlHttps "https://httpbin.org/get?foo=bar")
+    response <- req GET url NoReqBody jsonResponse $
+        "from" =: (15 :: Int)           <>
+        "to"   =: (67 :: Int)           <>
+        basicAuth "username" "password" <>
+        options                         <> -- contains the ?foo=bar part
+        port 443 -- here you can put any port of course
+    liftIO $ putStrLn "YES" -- print (responseBody response :: Value)
+
+yax = 
+    let 
+        myurl = Req.https  "www.abc.com" 
+        prm = Req.queryParam "foo" (Just 3 :: Maybe Int)
+    in 
+    9
+-}
+
+main :: IO ()
+main = runReq defaultHttpConfig $ do
+    let n, seed :: Int
+        n    = 5
+        seed = 100
+    bs <- req GET (https "httpbin.org" /: "bytes" /~ n) NoReqBody bsResponse $
+        "seed" =: seed
+    liftIO $ Char8.putStrLn "Hey ho" -- (responseBody bs)
