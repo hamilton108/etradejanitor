9a949e599d23afe88952fb189cc23c9c57071a94
Hamilton108 <hamilton@hamilton108.com>
2020-09-06 14:54:12 +0200

    Docker network pgutil_lofsrud in runMain

--- src/Demo.hs
+++ src/Demo.hs
@@ -2,6 +2,8 @@ module Demo where
 
 import Data.List.Split (splitOn)
 
+import qualified EtradeJanitor.Repos.Stocks as RS
+
 x = "a:b:c:x"
 
 lx = splitOn ":" x
@@ -9,3 +11,5 @@ lx = splitOn ":" x
 y = case lx of
     [v1,v2,v3] -> "Yep"
     _ -> "Nope"
+
+tix = RS.tickers "172.20.1.3"
